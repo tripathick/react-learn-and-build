56d24052b8b0eef3a8a9b34d778983bd4536a48e
tripathick <chandankumar765414@gmail.com>
2024-05-14 06:00:37 +0530

    eps-01

---
+++ App.js
@@ -0,0 +1,35 @@
+const heading = React.createElement("h1" , {} , "Hello World from React!!...");
+// Here createElement method takes 3 argument i.e (element  , object -> for providing attribute for
+// given tag/element , content inside given element)
+
+// We can also  create html DOM using createElement method as like
+//    <div>
+//       <div>
+//         <div>
+//             <h1>I am an h1 tag </h1>
+//                <h2>I am an h2 tag</h2>
+//         </div>
+//         <div>
+//             <h1>I am an h1 tag </h1>
+//                 <h2>I am an h2 tag</h2>
+//         </div>
+//       </div>
+//    </div>
+
+// How can we create this ?
+// We can create this using createElement() method/
+  const parent = React.createElement("div" , {id : "parent"} , [
+      React.createElement("div" , {id: "c1"} , [
+        React.createElement("h1" , {} , "I am h1 tag"),
+      React.createElement("h2" , {} , "I am an h2 tag"),
+      ]),
+      React.createElement("div" , {id: "c2"} , [
+        React.createElement("h1" , {} , "I am h1 tag"),
+      React.createElement("h2" , {} , "I am an h2 tag"),
+      ]),
+])  
+
+const root = ReactDOM. createRoot(document.getElementById("root"));
+
+// root.render(heading);
+root.render(parent);
